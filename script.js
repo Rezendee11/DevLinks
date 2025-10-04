f1be3a12943a5e24eb66f35edf00835a696d16c8
Rezendee11 <pedrorsantanna@gmail.com>
2025-10-03 23:12:17 -0300

    script.js

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 function toggleMode(){
-    
     const html = document.documentElement
+    const img = document.querySelector("#profile img")
     
+    // Verifica ANTES de trocar as classes
     if (html.classList.contains('dark')){
-        
-      html.classList.remove('dark')
-      html.classList.add('light')
-        
-    }else{
+        // Estava dark, vai virar light
+        html.classList.remove('dark')
+        html.classList.add('light')
+        img.setAttribute("src", "./assets/avatar-light.png")
+        img.setAttribute("alt", "Foto de Pedro Rezende de óculos e com moletom verde")
+    } else {
+        // Estava light, vai virar dark
         html.classList.remove('light')
         html.classList.add('dark')
+        img.setAttribute("src", "./assets/avatar.png")
+        img.setAttribute("alt", "Foto de Pedro Rezende com 16 anos de terno sorrindo")
     }
-
-//Pfp change
-const img = document.querySelector("#profile img")
-
-if (html.classList.contains("dark")){
-
-img.setAttribute("src", "./assets/avatar.png")
-img.setAttribute("alt", "Foto de Pedro Rezende com 16 anos  de terno sorrindo")
-    
-}else{
-    
-    img.setAttribute("src", "./assets/avatar-light.png")
-    img.setAttribute("alt", "Foto de Pedro Rezende de óculos e com moletom verde")
- }
 }
