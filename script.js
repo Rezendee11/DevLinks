b1e64209b46c7409762a564ae820e1bb32dc4ea5
Rezendee11 <pedrorsantanna@gmail.com>
2025-10-03 23:01:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const img = document.querySelector("#profile img")
 
 if (html.classList.contains("dark")){
 
-img.setAttribute("src", "./assets/Avatar.png")
+img.setAttribute("src", "./assets/avatar.png")
 img.setAttribute("alt", "Foto de Pedro Rezende com 16 anos  de terno sorrindo")
     
 }else{
